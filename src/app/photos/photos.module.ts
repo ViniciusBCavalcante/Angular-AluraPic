28cf2bf46c7aaf68312f6908ac1cfd61aa99e02f
ViniciusBCavalcante <vinicius.cavalcante05@gmail.com>
2022-02-24 16:50:00 -0300

    added mock api & photo service

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 
-import { PhotoComponent } from "./photo/photo.component";
+import { PhotoComponent } from './photo/photo.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-    declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ] 
+  declarations: [PhotoComponent],
+  exports: [PhotoComponent],
+  imports: [HttpClientModule],
 })
 export class PhotosModule {}
