9d19e6b756dc9616110236f0dffbd4a4c1bab953
ViniciusBCavalcante <vinicius.cavalcante05@gmail.com>
2022-02-28 17:11:26 -0300

    added filter by description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { AppRoutingModule } from './app-routing.module';
+import { ErrorsModule } from './errors/errors.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
@@ -5,14 +7,9 @@ import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    PhotosModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, PhotosModule, ErrorsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,11 +1,21 @@
+import { FilterByDescription } from './photo-list/filter-by-description.pipe';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 import { PhotoComponent } from './photo/photo.component';
-import { HttpClientModule } from '@angular/common/http';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 @NgModule({
-  declarations: [PhotoComponent],
-  exports: [PhotoComponent],
-  imports: [HttpClientModule],
+  declarations: [
+    PhotoComponent,
+    PhotoListComponent,
+    PhotoFormComponent,
+    PhotosComponent,
+    FilterByDescription,
+  ],
+  imports: [HttpClientModule, CommonModule],
 })
 export class PhotosModule {}
